bae0d21bff0d32b0b97d273d4e71755d25b44ac7
AndyBeable <andy.beable@gmail.com>
2021-12-06 18:05:22 +0000

    map marker

--- src/components/location/Map.vue
+++ src/components/location/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
+  <l-map style="height: 500px" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker :lat-lng="markerLatLng"></l-marker>
   </l-map>
@@ -21,8 +21,8 @@ export default {
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
+      center: [41.481310, -71.310410],
+      markerLatLng: [41.481310, -71.310410]
     };
   }
 }
